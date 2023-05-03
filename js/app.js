75612423753de8b8a3865b314e6994a99429e4c4
marcos <masadu@outlook.es>
2023-05-03 13:49:04 +0200

    slider

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
+let indexPhoto = 1
 const pictures = [
   "contBcg-0",
   "contBcg-1",
   "contBcg-2",
   "contBcg-3",
-  "contBcg-4"
+  "contBcg-4",
+  
 ];
 
 // buena suerte
+
+
+
+document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+
+document.querySelector(".btn-left").addEventListener("click",() => {
+  indexPhoto = (indexPhoto - 1 + pictures.length) % pictures.length;
+  document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+
+
+})
+
+  document.querySelector(".btn-right").addEventListener("click", () => {
+    indexPhoto = (indexPhoto + 1 ) % pictures.length;
+    document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+  });
+
